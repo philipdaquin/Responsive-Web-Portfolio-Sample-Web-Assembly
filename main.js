af9c9ae029c07a0452fb9b9101f7c08259decb62
philidaquin <philipdaquin20@gmail.com>
2022-01-18 18:19:01 +0800

    Converting to Yew Componetns

--- main.js
+++ main.js
@@ -95,4 +95,22 @@ const swiper = new Swiper('.testimonial__container', {
         },
 
     }
-  });
+});
+gsap.from('.home__img', { 
+    opacity: 0, duration: 2, delay: .5, x: 60
+})
+gsap.from('.home__data', { 
+    opacity: 0, duration: 2, delay: .8, y: 25
+})
+gsap.from('.home__greeting, .home__name, home__profession, .home__button', { 
+    opacity: 0, duration: 2, delay: 1, y: 25, ease: 'expo.out', stagger: .2
+})
+gsap.from('.nav__logo, .nav__toggle', { 
+    opacity: 0, duration: 2, delay: 1.5, y: 25, ease: 'expo.out', stagger: .2
+})
+gsap.from('.nav__item', { 
+    opacity: 0, duration: 2, delay: 1.8, y: 25, ease: 'expo.out', stagger: .2
+})
+gsap.from('.home__social-icon', { 
+    opacity: 0, duration: 2, delay: 1.8, y: 25, ease: 'expo.out', stagger: .2
+})
